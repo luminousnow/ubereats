efff47229cb671399dabed9f25602e98f0296a49
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2022-03-09 13:01:37 +0200

    add filter.js

---
+++ js/filter.js
@@ -0,0 +1,5 @@
+const filterButtonList = document.querySelectorAll(".category__item");
+console.log("TCL: filterButtonList", filterButtonList);
+
+const dishList = document.querySelector(".restaurant__dish-list .row");
+console.log("TCL: dishList", dishList);
